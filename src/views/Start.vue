28e8f296c0a3a561802603cd7c61da3fe00f17fc
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-12-21 20:00:14 +0100

    Add tailwind configuration

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,21 +1,12 @@
 <template>
-    <h1>Start layout</h1>
+    <h1 class="text-sm text-gray-500">Start layout</h1>
     <h1></h1>
     <div>
-        <h2>
+        <h2 class="text-red-500">
             test
         </h2>
     </div>
 </template>
-<script setup lang="ts"></script>
-<style scoped lang="scss">
-h1 {
-    color: red;
-}
-
-div {
-    h2 {
-        color: green;
-    }
-}
-</style> 
+<script setup lang="ts">
+</script>
+<style scoped lang="scss"></style>
