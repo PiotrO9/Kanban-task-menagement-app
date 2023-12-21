411f1e886bd0322be3c3bd28b3bfa9bdcadb5e71
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-12-21 19:48:41 +0100

    Add base configuration

---
+++ src/views/Start.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Start layout</h1>
+    <h1></h1>
+    <div>
+        <h2>
+            test
+        </h2>
+    </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="scss">
+h1 {
+    color: red;
+}
+
+div {
+    h2 {
+        color: green;
+    }
+}
+</style> 
